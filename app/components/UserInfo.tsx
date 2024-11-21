7b2021e52b8a277808ccef3ee8f045720cbcb9ad
timmchi <tnmagg@gmail.com>
2024-11-21 16:42:12 +0200

    user profile

---
+++ app/components/UserInfo.tsx
@@ -0,0 +1,43 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+
+const UserInfo = () => {
+  return (
+    <View style={styles.userInfoContainer}>
+      <Image
+        source={{
+          uri: "https://flowbite.com/docs/images/people/profile-picture-4.jpg",
+        }}
+        style={styles.image}
+      />
+      <View style={styles.userInfo}>
+        <View>
+          <Text style={styles.userInfoCount}>13</Text>
+          <Text>posts</Text>
+        </View>
+        <View>
+          <Text style={styles.userInfoCount}>500</Text>
+          <Text>followers</Text>
+        </View>
+        <View>
+          <Text style={styles.userInfoCount}>300</Text>
+          <Text>following</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default UserInfo;
+
+const styles = StyleSheet.create({
+  userInfoContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 30,
+  },
+  image: { width: 80, height: 80, borderRadius: 40 },
+  userInfoCount: { textAlign: "center", fontWeight: "bold", fontSize: 16 },
+  userInfo: { flexDirection: "row", gap: 60 },
+});
--- app/screens/ProfileScreen.tsx
+++ app/screens/ProfileScreen.tsx
@@ -2,17 +2,14 @@ import { View, Text } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Button } from "@react-navigation/elements";
+import UserProfile from "../components/UserProfile";
 
 const ProfileScreen = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>ProfileScreen</Text>
-      <Button onPress={() => navigation.navigate("WelcomeScreen")}>
-        To Welcome Screen
-      </Button>
-      <Button onPress={() => navigation.goBack()}>Go back</Button>
+    <View style={{ flex: 1 }}>
+      <UserProfile />
     </View>
   );
 };
